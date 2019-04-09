6092bf2fa98728809c8e4a6d14bdee2240c60728
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-04-09 01:02:48 +0100

    2345123490

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 import PlayerBar from "./components/PlayerBar";
 import SearchBar from "./components/SearchBar";
-console.log(SearchBar);
 
 const App = () => {
   return (
     <div className="app">
       <div className="app--name" id="app--name">
-        <p>SoundReacter is awesome</p>
+        <p>SoundReact</p>
         <svg className="app__name--svg" id="app__name--svg">
           <use xlinkHref="./img/sprite.svg#icon-react" />
         </svg>
